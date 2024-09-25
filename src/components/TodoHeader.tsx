9ae48b131733d033f54d6d2354d8d128b6f18e9c
sovakrya <tyan_34ee@mail.ru>
2024-09-25 16:45:21 +0300

    add TodoHeader

--- src/components/TodoHeader.tsx
+++ src/components/TodoHeader.tsx
@@ -1,42 +1,38 @@
-import React, { createRef, RefObject } from "react";
+import React, { createRef, MutableRefObject, RefObject, useRef } from "react";
 import "../styles/TodoHeader.css";
 import { addTodo, Todo } from "../service/todo";
 
-export default class TodoHeader extends React.Component<{
-  onAddedTodo: (todo: Todo) => void;
-}> {
-  inputRef: RefObject<HTMLInputElement> = createRef();
+export default function TodoHeader(onAddedTodo: (todo: Todo) => void) {
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  addTodoFromFetch = () => {
-    if (!this.inputRef.current) {
+  const addTodoFromFetch = () => {
+    if (!inputRef.current) {
       return;
     }
     addTodo({
-      title: this.inputRef.current.value,
+      title: inputRef.current.value,
       completed: false,
       userId: 1,
-    }).then(this.props.onAddedTodo);
+    }).then(onAddedTodo);
 
-    this.inputRef.current.value = "";
+    inputRef.current.value = "";
   };
 
-  render(): React.ReactNode {
-    return (
-      <div className="header-main-box">
-        <h1 className="header-title">Todo list</h1>
+  return (
+    <div className="header-main-box">
+      <h1 className="header-title">Todo list</h1>
 
-        <div className="header-actions-box">
-          <input
-            className="header-input"
-            type="text"
-            placeholder="Например: погладить кота"
-            ref={this.inputRef}
-          />
-          <button className="header-btn" onClick={this.addTodoFromFetch}>
-            Добавить
-          </button>
-        </div>
+      <div className="header-actions-box">
+        <input
+          className="header-input"
+          type="text"
+          placeholder="Например: погладить кота"
+          ref={inputRef}
+        />
+        <button className="header-btn" onClick={addTodoFromFetch}>
+          Добавить
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
