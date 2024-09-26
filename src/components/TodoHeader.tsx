a665ccea35bb40fc747fe895931f474ae7e69ca2
sovaKrya <tyan_34ee@mail.ru>
2024-09-26 09:54:51 +0300

    add styled components in project and stylize TodoHeader with styled components

--- src/components/TodoHeader.tsx
+++ src/components/TodoHeader.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import "../styles/TodoHeader.css";
 import { addTodo, Todo } from "../service/todo";
+import styled from "styled-components";
 
 export default function TodoHeader(props: {
   onAddedTodo: (todo: Todo) => void;
@@ -20,21 +20,61 @@ export default function TodoHeader(props: {
     setTodoTitle("");
   };
 
-  return (
-    <div className="header-main-box">
-      <h1 className="header-title">Todo list</h1>
+  const HeaderMainBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    padding: 24px;
+    border-radius: 16px;
+    box-shadow: 6px 6px 12px rgb(199, 199, 199);
+    align-items: center;
+    width: 500px;
+  `;
 
-      <div className="header-actions-box">
-        <input
-          className="header-input"
+  const HeaderTitle = styled.h1`
+    font-size: 30px;
+    font-weight: 600;
+  `;
+
+  const HeaderAcrionsBox = styled.div`
+    display: flex;
+    gap: 6px;
+    width: 100%;
+  `;
+
+  const HeadetInput = styled.input`
+    flex-grow: 1;
+    height: 28px;
+    border: solid 1px rgb(180, 180, 180);
+    border-radius: 5px;
+  `;
+
+  const HeaderBtn = styled.button`
+    background: none;
+    border-radius: 5px;
+    border: solid 2.5px rgb(162, 92, 209);
+    cursor: pointer;
+    font-size: 14px;
+    transition: ease-in-out 0.5s;
+
+    &:hover {
+    background-color: rgb(162, 92, 209);
+    color: aliceblue;
+    }
+  `;
+
+  return (
+    <HeaderMainBox>
+      <HeaderTitle>Todo list</HeaderTitle>
+
+      <HeaderAcrionsBox>
+        <HeadetInput
           type="text"
           placeholder="Например: погладить кота"
           onChange={(e) => setTodoTitle(e.target.value)}
         />
-        <button className="header-btn" onClick={addTodoFromFetch}>
-          Добавить
-        </button>
-      </div>
-    </div>
+        <HeaderBtn onClick={addTodoFromFetch}>Добавить</HeaderBtn>
+      </HeaderAcrionsBox>
+    </HeaderMainBox>
   );
 }
